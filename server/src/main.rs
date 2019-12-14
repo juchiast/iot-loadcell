f8486abc3c01f10e8d633810bd29a5a93bba8ef7
Duy Do <juchiast@gmail.com>
2019-12-14 15:24:38 +0700

    Read serial

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,20 @@
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let mut ser =
+        serialport::posix::TTYPort::open(Path::new("/dev/ttyUSB0"), &Default::default()).unwrap();
+    use std::io::Read;
+    use std::io::Write;
+    let mut buf = [0u8; 256];
+    loop {
+        while let Ok(size) = ser.read(&mut buf) {
+            if size == 0 {
+                break;
+            }
+            let s = std::str::from_utf8(&buf[..size]);
+            if let Ok(s) = s {
+                println!("{}", s);
+            }
+        }
+    }
 }
