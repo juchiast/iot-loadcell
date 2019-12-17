7c279adcf52b0efa27febf9bf9596a71a127f72d
Duy Do <juchiast@gmail.com>
2019-12-17 12:00:43 +0700

    tty discover

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,29 @@
-use std::path::Path;
+#[macro_use]
+extern crate log;
 
+mod error;
+mod tty_discover;
+
+use futures::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let mut rx = tty_discover::tty_discorver();
+
+    while let Some(name) = rx.next().await {
+        println!("{:?}", name);
+    }
+}
+
+/*
 fn main() {
     let mut ser =
         serialport::posix::TTYPort::open(Path::new("/dev/ttyUSB0"), &Default::default()).unwrap();
     use std::io::Read;
     use std::io::Write;
     let mut buf = [0u8; 256];
-    let mut y: f64 = 0;
-    let alpha = 0.1;
     loop {
         while let Ok(size) = ser.read(&mut buf) {
             if size == 0 {
@@ -21,3 +37,4 @@ fn main() {
         }
     }
 }
+*/
---
+++ server/src/tty_discover.rs
@@ -0,0 +1,44 @@
+use futures::future;
+use serialport::posix::TTYPort;
+use std::path::PathBuf;
+use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::time as timer;
+
+use futures::prelude::*;
+
+pub fn tty_discorver() -> UnboundedReceiver<Vec<PathBuf>> {
+    let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+    tokio::spawn(async move {
+        let mut interval = timer::interval(timer::Duration::from_secs(1));
+        let mut previous_ports: Option<Vec<PathBuf>> = None;
+        while let Some(_) = interval.next().await {
+            let ports = serialport::available_ports()
+                .map_err(|e| warn!("Error: {}", e))
+                .unwrap_or_default();
+            let ports = future::join_all(
+                ports
+                    .into_iter()
+                    .map(|port| test_port(port.port_name.into())),
+            )
+            .await
+            .into_iter()
+            .filter_map(Result::ok)
+            .collect();
+            if previous_ports.as_ref() != Some(&ports) {
+                tx.send(ports.clone()).unwrap();
+                previous_ports = Some(ports);
+            }
+        }
+    });
+
+    rx
+}
+
+async fn test_port(path: PathBuf) -> Result<PathBuf, ()> {
+    tokio::task::spawn_blocking(move || -> Result<PathBuf, ()> {
+        TTYPort::open(&path, &Default::default()).map_err(|_| ())?;
+        Ok(path)
+    })
+    .await
+    .unwrap_or(Err(()))
+}
